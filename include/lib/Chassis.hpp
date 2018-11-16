8ebd0d2446e74d7afd413674b2a0111bcad5a64e
Gavin You <gavinyou082@gmail.com>
2018-11-16 15:22:16 +0800

    Removed okapilib

---
+++ include/lib/Chassis.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "pros/motors.hpp"
+#include <vector>
+
+using namespace pros;
+using namespace std;
+
+class Chassis{
+public:
+  double ForwardCoefficient = 1, TurningCoefficient = 1;
+  int MaxSpeed = 127;
+  bool IsReversed = false;
+  int CurrentSpeed, CurrentYaw;
+  vector<Motor> LeftMotors, RightMotors;
+
+  Chassis(const vector<Motor> left, const vector<Motor> right, const int &maxSpeed);
+  void Drive(const int &forward, const int &yaw);
+  void Stop();
+  void SetMotorLeft(const int &speed);
+  void SetMotorRight(const int &speed);
+
+private:
+  double _maxSpeed;
+};
---
+++ src/lib/Chassis.cpp
@@ -0,0 +1,48 @@
+#include "lib/Chassis.hpp"
+#include <vector>
+
+int limit(int limit, int value){
+	if (value < -limit) {
+		return -limit;
+	} else if (value > limit) {
+		return limit;
+	} else {
+		return value;
+	}
+}
+
+Chassis::Chassis(const vector<Motor> left, const vector<Motor> right, const int &maxSpeed){
+  for(Motor mtr:left){
+    LeftMotors.push_back(mtr);
+  }
+  for(Motor mtr:right){
+    RightMotors.push_back(mtr);
+  }
+  MaxSpeed = maxSpeed;
+}
+
+void Chassis::Drive(const int &forward, const int &yaw){
+  int realforward = forward * (IsReversed ? 1 : -1);
+	int left = ForwardCoefficient * realforward + TurningCoefficient * yaw;
+  int right = ForwardCoefficient * realforward - TurningCoefficient * yaw;
+
+	SetMotorLeft(limit(127, left));
+	SetMotorRight(limit(127, right));
+}
+
+void Chassis::Stop(){
+  SetMotorLeft(0);
+  SetMotorRight(0);
+}
+
+void Chassis::SetMotorLeft(const int &speed){
+  for(Motor mtr:LeftMotors){
+    mtr.move(speed);
+  }
+}
+
+void Chassis::SetMotorRight(const int &speed){
+  for(Motor mtr:RightMotors){
+    mtr.move(speed);
+  }
+}
