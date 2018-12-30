b2c54885159e36f3002f12f987600d899307d85f
Gavin You <youjincheng2002@163.com>
2018-12-30 23:12:38 +0800

    Clean up

--- include/test.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "lib/chassis.hpp"
-
-class Test{
-public:
-  static void ChassisTest(Chassis &chassis);
-  static void ControllerEventTest();
-};
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -25,6 +25,7 @@ using namespace pros;
 
 #define reverse true
 
+//configuration
 Motor left_f_mtr(15, MOTOR_GEARSET_18, reverse);
 Motor left_b_mtr(20, MOTOR_GEARSET_18, reverse);
 Motor right_f_mtr(1, MOTOR_GEARSET_18);
@@ -34,30 +35,23 @@ Motor collector(11, MOTOR_GEARSET_36);
 Motor shoot1(17, MOTOR_GEARSET_6);
 Motor shoot2(19, MOTOR_GEARSET_6, reverse);
 
-Chassis chassis({ left_f_mtr, left_b_mtr }, { right_f_mtr, right_b_mtr });
-
-int test=0;
+int test = 0;
 
 void callback_test() {
 	pros::lcd::print(2, "callback test %d", ++test);
-	chassis.Drive(50,0);
+	chassis.Drive(50, 0);
 	delay(1000);
 	chassis.Stop();
 }
 
 void opcontrol() {
+	//initialization
+	Chassis chassis({ left_f_mtr, left_b_mtr }, { right_f_mtr, right_b_mtr });
 	Controller master(pros::E_CONTROLLER_MASTER);
-	//SmartController master_smart();
 
 	Button b1(master, DIGITAL_L1);
 	b1.SetClickedEvent(callback_test);
 	EventHandler::EnableButtonEvents();
-
-#ifdef UNIT_TEST
-	//Test::ChassisTest(chassis);
-	//Test::ControllerEventTest();
-	return;
-#endif
 
 	while (true) {
 		int forward = master.get_analog(ANALOG_LEFT_Y);
@@ -66,19 +60,21 @@ void opcontrol() {
 		chassis.Drive(forward, yaw);
 		pros::lcd::print(0, "Is KeyA down %d", master.get_digital(DIGITAL_A));
 
-		if(master.get_digital(DIGITAL_A)){
+		if (master.get_digital(DIGITAL_A)) {
 			collector.move(127);
-		}else if(master.get_digital(DIGITAL_B)){
+		}
+		else if (master.get_digital(DIGITAL_B)) {
 			collector.move(-127);
-		}else{
+		}
+		else {
 			collector.move(0);
 		}
 
-
-		if(master.get_digital(DIGITAL_X)){
+		if (master.get_digital(DIGITAL_X)) {
 			shoot1.move(100);
 			shoot2.move(100);
-		}else{
+		}
+		else {
 			shoot1.move(0);
 			shoot2.move(0);
 		}
