34a7fcd6767dc85ae62affd54c6862baff850976
Jiaxun Yang <jiaxun.yang@flygoat.com>
2019-01-27 11:03:23 +0800

    Add _set_delay
    Delay in a statement machine.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- include/lib/auto_move.hpp
+++ include/lib/auto_move.hpp
@@ -11,7 +11,7 @@ extern uint32_t move_start_time[64];
 #define MOV_ALREADY_DONE 2
 
 #define _set_movement(MOV_VELOCITY, MOV_LEFT, MOV_RIGHT, MOV_TIME)      \
-do {\
+do {  \
   movecnt++;   \
   if (move_state[movecnt] == MOV_RUNNING){\
     if (move_start_time[movecnt] - millis() >= MOV_TIME){\
@@ -27,6 +27,23 @@ do {\
     chassis.ClearEncoderR();\
     chassis.SetMotorsRelativeL(MOV_LEFT, MOV_VELOCITY);\
     chassis.SetMotorsRelativeR(MOV_RIGHT, MOV_VELOCITY);\
+  }\
+  __next:;\
+} while (0);
+
+#define _set_delay(x) \
+do {  \
+  movecnt++;   \
+  if (move_state[movecnt] == MOV_RUNNING){\
+    if (move_start_time[movecnt] - millis() >= x){\
+      move_state[movecnt] = MOV_ALREADY_DONE;\
+    }\
+    goto __end;\
+  }\
+  if (move_state[movecnt] == MOV_ALREADY_DONE){ goto __next; }\
+  if (move_state[movecnt] == MOV_FIRST_RUN){\
+    move_state[movecnt] = MOV_RUNNING;\
+    move_start_time[movecnt] = millis();\
   }\
   __next:;\
 } while (0);
