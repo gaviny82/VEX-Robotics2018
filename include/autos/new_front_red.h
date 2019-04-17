471272731ab743bab76ee9f2b9b0cde1c88f60a4
lizhejun <1220047112@qq.com>
2019-04-18 00:09:08 +0800

    Add same new program of front_red. Also need change the variables. change the printed name of new_front_blue.h on the master.

--- include/autos/new_front_red.h
+++ include/autos/new_front_red.h
@@ -1,11 +1,11 @@
-pros::lcd::print(1, "Auto: new_front_blue");
+pros::lcd::print(1, "Auto: new_front_red");
 
 //shoot
 _set_onetime_task(127, ShootSignal = SIG_SHOOT);
 _set_onetime_task(0, arm.move(127));
 
 //turning
-_set_onetime_task(500, chassis.Drive(-50,50));//135 degree
+_set_onetime_task(500, chassis.Drive(50,-50));//135 degree
 _set_onetime_task(500, chassis.Drive(0,0));
 
 //straight
@@ -22,7 +22,7 @@ _set_movement(127, -400, -400, 800);
 _set_onetime_task(0, collector.move(0));
 
 //turning
-_set_onetime_task(500,chassis.Drive(50,-50));//135 degree
+_set_onetime_task(500,chassis.Drive(-50,50));//135 degree
 _set_onetime_task(0,chassis.Drive(0,0));
 
 //straight
@@ -34,7 +34,7 @@ _set_movement(127, 500, 500, 1000);
 _set_movement(127,-1000, -1000, 2000);
 
 //turning
-_set_onetime_task(1000, chassis.Drive(-100, 100));
+_set_onetime_task(1000, chassis.Drive(100, -100));
 _set_onetime_task(0, chassis.Drive(0,0));
 
 //straight
@@ -43,7 +43,7 @@ _set_movement(127, 1000, 1000, 2000);
 _set_movement(127, -100, -100, 200);
 
 //turning
-_set_onetime_task(500, chassis.Drive(-50, 50))//45 degree
+_set_onetime_task(500, chassis.Drive(50, -50))//45 degree
 _set_onetime_task(0, chassis.Drive(0,0));
 _set_onetime_task(0, collector.move(0));
 
@@ -51,7 +51,7 @@ _set_onetime_task(0, collector.move(0));
 _set_movement(127, -700, -700, 1500);
 
 //turning
-_set_onetime_task(500, chassis.Drive(50, -50));//90 degree
+_set_onetime_task(500, chassis.Drive(-50, 50));//90 degree
 
 
 //changeable
