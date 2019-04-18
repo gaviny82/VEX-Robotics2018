422f558bc52d5008a63e065b406b2de93101a862
lizhejun <1220047112@qq.com>
2019-04-18 12:34:07 +0800

    change the plan of two front auto plan.

--- include/autos/new_front_blue.h
+++ include/autos/new_front_blue.h
@@ -1,33 +1,31 @@
 pros::lcd::print(1, "Auto: new_front_blue");
 
-//shoot
-_set_onetime_task(500, ShootSignal = SIG_SHOOT);
-_set_onetime_task(0, arm.move(127));
-
-//turning
-_set_onetime_task(500, chassis.Drive(-50,50));//135 degree
-_set_onetime_task(500, chassis.Drive(0,0));
-
 //straight
-_set_onetime_task(0, arm.move(0);//需要保证在到达球后面之前arm不要落地
+_set_onetime_task(0, arm.move(127));//changeable
+_set_onetime_task(0, collector.move(127));
 _set_movement(127, 500, 500, 1000);
-_set_onetime_task(500, arm.move(127))；
+_set_onetime_task(500, arm.move(127));
 _set_onetime_task(0, arm.move(0));
 _set_movement(127, -200, -200, 400);
 
 //back
-_set_onetime_task(0, collector.move(127));
-_set_movement(127, 100, 100, 200);
-_set_movement(127, -400, -400, 800);
+_set_movement(127, 200, 200, 400);
+_set_movement(127, -500, -500, 800);
 _set_onetime_task(0, collector.move(0));
 
 //turning
 _set_onetime_task(500,chassis.Drive(50,-50));//135 degree
 _set_onetime_task(0,chassis.Drive(0,0));
 
+//shoot
+_set_onetime_task(500, ShootSignal = SIG_SHOOT);
+_set_onetime_task(0, collector.move(127));
+
 //straight
 _set_movement(127, 500, 500, 1000);
-_set_onetime_task(127,ShootSignal = SIG_SHOOT);
+
+//shoot again
+_set_onetime_task(500, ShootSignal = SIG_SHOOT);
 
 //Third flag
 _set_movement(127, 500, 500, 1000);
@@ -36,10 +34,11 @@ _set_movement(127,-1000, -1000, 2000);
 //turning
 _set_onetime_task(1000, chassis.Drive(-100, 100));
 _set_onetime_task(0, chassis.Drive(0,0));
+_set_onetime_task(500,chassis.move(-127,-127));
+_set_onetime_task(0, chassis.move(0, 0));
 
 //straight
-_set_onetime_task(0, collector.move(127));
-_set_movement(127, 1000, 1000, 2000);
+_set_movement(127, 1200, 1200, 2400);
 _set_movement(127, -100, -100, 200);
 
 //turning
@@ -52,6 +51,7 @@ _set_movement(127, -700, -700, 1500);
 
 //turning
 _set_onetime_task(500, chassis.Drive(50, -50));//90 degree
+_set_onetime_task(0, chassis.Drive(0, 0));
 
 
 //changeable
