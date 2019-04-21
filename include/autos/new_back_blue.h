be6ed00cbb7842cc93621930e1d8a0d486b171cd
Jiaxun Yang <jiaxun.yang@flygoat.com>
2019-04-21 10:47:12 +0800

    Back Blue Struc

--- include/autos/new_back_blue.h
+++ include/autos/new_back_blue.h
@@ -1,58 +1,25 @@
 pros::lcd::print(1, "Auto: new_back_blue");
 
-//shoot
-_set_onetime_task(500, ShootSignal = SIG_SHOOT);
+//轮子第二格
 
-//fetch a ball
-_set_onetime_task(0, arm_m = -90);//changeable
-_set_onetime_task(0, collector.move(127));
-_set_movement(90, 1400, 1400, 1400);
-_set_onetime_task(0, arm_m = 0);
-_set_movement(90, -200, -200, 400);
-
-//back
-_set_movement(90, 200, 200, 600);
-
-_set_onetime_task(0, arm_m = 90);
-_set_onetime_task(0, collector.move(0));
-_set_movement(90, -1300, -1300, 1400);
-_set_onetime_task(0, arm_m = 0);
-
-//turning
-_set_onetime_task(150, chassis.Drive(0, -127));
-_set_onetime_task(0, chassis.Drive(0, 0));
-
-//straight
-_set_onetime_task(0, collector.move(127));
-_set_movement(127, 3500, 3500, 3500);
-_set_movement(127, -400, -400, 400);
-
-//turning
-_set_onetime_task(450, chassis.Drive(0, 127));
-_set_onetime_task(0, collector.move(0));
-_set_onetime_task(0, chassis.Drive(0,0));
-
-
-//backward & forward
-_set_movement(127, -2200, -2200, 2200);
-_set_movement(127, 2200, 2200, 2200);
-_set_onetime_task(0, collector.move(0));
-
-//turning
-_set_onetime_task(100, chassis.Drive(0, 127));
-_set_onetime_task(0, chassis.Drive(0, 0));
-
-//go up
-_set_movement(127, 2000, 2000, 2000);
-
-
-//fact
-/*
-_set_movement(127, 1000, 1000, 2000);//(motor, left, right, time)
-_set_onetime_task(1000, collector.move(127));//timedelay, move collector
-_set_onetiem_task(0, chassis.Drive(-127,127));//timedelay, left motor,right motor
-_set_onetime_task(1000, collector.move(0));//stop collector
-_set_onetime_task(0, chassis.Drive(0, 0));//stop left and right motor
-_set_onetime_task(1000, ShootSignal = SIG_SHOOT);//shoot
-_set_onetime_task(1000, arm_m = -127);
-*/
+_set_onetime_task(0,collector.move(110));//开始吸球
+_set_movement_warm(75,1200,1200,3000);//到射球位
+_set_movement(100,590,-590,900);//转向对旗
+_set_onetime_task(500, ShootSignal = SIG_SHOOT);/*中间高旗射球*/
+_set_movement_warm(100,-1850,-1850,3000);//后退
+_set_movement(100,870,-870,800);//转向对盘
+_set_onetime_task(0,_arm_pan);
+_set_movement_warm(75,-2400,-2400,3000);//怼盘子
+_set_movement_warm(110,2500,2500,3000); //怼完跑路
+_set_movement(100,-630,630,800);//转向撞墙
+_set_onetime_task(500, chassis.SetMotorsLeft(-80); chassis.SetMotorsRight(-80));
+_set_onetime_task(0, chassis.SetMotorsLeft(0); chassis.SetMotorsRight(0));
+_set_movement_warm(100,1500,1500,3000);//到盘子旁边
+_set_movement(100,-720,720,800);//转向对盘
+_set_movement_warm(75,2300,2300,3000);//向前顶盘
+_set_delayed_task(800, _arm_up); /*收arm*/
+_set_movement_warm(100,-600,-600,3000);//后退
+_set_movement(100,630,-630,800);//转向对台
+_set_onetime_task(1000, chassis.SetMotorsLeft(127); chassis.SetMotorsRight(127));
+_set_onetime_task(100, chassis.SetMotorsLeft(-10); chassis.SetMotorsRight(-10));
+_set_onetime_task(100, chassis.SetMotorsRelativeL(0, 30); chassis.SetMotorsRelativeR(0,30));
